d3504e8515d57f1b90066d3de606506f777e94b5
Thamizharasu <zarub2k@gmail.com>
2016-05-01 00:39:02 +0530

    Fix: Un-used imports are removed

--- src/main/java/com/cloudskol/restc/client/RestClient.java
+++ src/main/java/com/cloudskol/restc/client/RestClient.java
@@ -7,9 +7,6 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Form;
-import javax.ws.rs.core.MultivaluedHashMap;
-import javax.ws.rs.core.MultivaluedMap;
 import java.util.List;
 
 /**
